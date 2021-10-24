6a74e41c861459096a8536309f7f9433f552f983
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-10-24 21:06:36 +0700

    Added dashboard and send endpoints

--- src/main/java/id/go/lan/pusaka/ikksurvey/controller/EnumeratorController.java
+++ src/main/java/id/go/lan/pusaka/ikksurvey/controller/EnumeratorController.java
@@ -6,12 +6,14 @@ import id.go.lan.pusaka.ikksurvey.service.KebijakanService;
 import id.go.lan.pusaka.ikksurvey.utility.ModelMapperUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/kebijakan")
@@ -28,6 +30,85 @@ public class EnumeratorController {
         Kebijakan kebijakan = kebijakanService.findById(idKebijakan);
         KebijakanDto kebijakanDto = modelMapperUtility.initialize().map(kebijakan, KebijakanDto.class);
 
+        Double progres = BigDecimal.valueOf(kebijakan.getKebijakanDetail().getProgres())
+                .setScale(2, BigDecimal.ROUND_HALF_DOWN)
+                .doubleValue();
+
+        kebijakanDto.setProgres(progres);
+        return kebijakanDto;
+    }
+
+    @GetMapping("/enumerator/dashboard/masuk")
+    @PreAuthorize("hasAnyAuthority('role_enumerator')")
+    public List<KebijakanDto> findKebijakanByEnumeratorAndStatusDisetujui() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        List<Kebijakan> kebijakanList = kebijakanService.findByEnumeratorAndStatus(currentPrincipalName, "disetujui");
+
+        List<KebijakanDto> kebijakanDtoList = new ArrayList<>();
+        for (Kebijakan kebijakan : kebijakanList) {
+            KebijakanDto kebijakanDto = modelMapperUtility.initialize().map(kebijakan, KebijakanDto.class);
+
+            Double progres = BigDecimal.valueOf(kebijakan.getKebijakanDetail().getProgres())
+                    .setScale(2, BigDecimal.ROUND_HALF_DOWN)
+                    .doubleValue();
+
+            kebijakanDto.setProgres(progres);
+
+            kebijakanDtoList.add(kebijakanDto);
+        }
+        return kebijakanDtoList;
+    }
+
+    @GetMapping("/enumerator/dashboard/proses")
+    @PreAuthorize("hasAnyAuthority('role_enumerator')")
+    public List<KebijakanDto> findKebijakanByEnumeratorAndStatusProses() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        List<Kebijakan> kebijakanList = kebijakanService.findByEnumeratorAndStatus(currentPrincipalName, "proses");
+
+        List<KebijakanDto> kebijakanDtoList = new ArrayList<>();
+        for (Kebijakan kebijakan : kebijakanList) {
+            KebijakanDto kebijakanDto = modelMapperUtility.initialize().map(kebijakan, KebijakanDto.class);
+
+            Double progres = BigDecimal.valueOf(kebijakan.getKebijakanDetail().getProgres())
+                    .setScale(2, BigDecimal.ROUND_HALF_DOWN)
+                    .doubleValue();
+
+            kebijakanDto.setProgres(progres);
+
+            kebijakanDtoList.add(kebijakanDto);
+        }
+        return kebijakanDtoList;
+    }
+
+    @PostMapping("/enumerator/mulai/{idKebijakan}")
+    @PreAuthorize("hasAnyAuthority('role_enumerator')")
+    public KebijakanDto startKebijakanEnumeratorByIdKebijakan(
+            @PathVariable("idKebijakan") Long idKebijakan) {
+        Kebijakan kebijakan = kebijakanService.findById(idKebijakan);
+        kebijakan.setStatus("proses");
+        Kebijakan savedKebijakan = kebijakanService.save(kebijakan);
+
+        KebijakanDto kebijakanDto = modelMapperUtility.initialize().map(savedKebijakan, KebijakanDto.class);
+        Double progres = BigDecimal.valueOf(kebijakan.getKebijakanDetail().getProgres())
+                .setScale(2, BigDecimal.ROUND_HALF_DOWN)
+                .doubleValue();
+
+        kebijakanDto.setProgres(progres);
+        return kebijakanDto;
+    }
+
+    @PostMapping("/enumerator/kirim/{idKebijakan}")
+    @PreAuthorize("hasAnyAuthority('role_enumerator')")
+    public KebijakanDto sendKebijakanToAdminInstansiByIdKebijakan(
+            @PathVariable("idKebijakan") Long idKebijakan) {
+        Kebijakan kebijakan = kebijakanService.findById(idKebijakan);
+        kebijakan.setIsSentByEnumerator(true);
+        kebijakan.setSentByEnumeratorAt(new Date());
+        Kebijakan savedKebijakan = kebijakanService.save(kebijakan);
+
+        KebijakanDto kebijakanDto = modelMapperUtility.initialize().map(savedKebijakan, KebijakanDto.class);
         Double progres = BigDecimal.valueOf(kebijakan.getKebijakanDetail().getProgres())
                 .setScale(2, BigDecimal.ROUND_HALF_DOWN)
                 .doubleValue();
