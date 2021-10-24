8524f24f2e2759ce0f5f93799c874924ebf28d60
azemoning <ahmadsaufimaulana@gmail.com>
2021-10-24 10:10:18 +0700

    fix duplicate bean

--- src/main/java/id/go/lan/pusaka/ikksurvey/service/impl/DashboardKoordinatorInstansiServiceImpl.java
+++ src/main/java/id/go/lan/pusaka/ikksurvey/service/impl/DashboardKoordinatorInstansiServiceImpl.java
@@ -1,13 +1,18 @@
 package id.go.lan.pusaka.ikksurvey.service.impl;
 
 import id.go.lan.pusaka.ikksurvey.model.dto.dashboard.DashboardAdminInstansiCardDto;
-import id.go.lan.pusaka.ikksurvey.service.DashboardAdminInstansiService;
+import id.go.lan.pusaka.ikksurvey.repository.KebijakanRepository;
+import id.go.lan.pusaka.ikksurvey.service.DashboardKoordinatorInstansiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class DashboardKoordinatorInstansiServiceImpl implements DashboardAdminInstansiService {
+public class DashboardKoordinatorInstansiServiceImpl implements DashboardKoordinatorInstansiService {
+    @Autowired
+    private KebijakanRepository kebijakanRepository;
+
     @Override
-    public DashboardAdminInstansiCardDto getAdminInstansiDashboardCardData() {
+    public DashboardAdminInstansiCardDto getKoordinatorInstansiDashboardCardData() {
         return null;
     }
 }
